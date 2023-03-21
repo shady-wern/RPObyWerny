53ef458a751e6cd8b6ce4bebca66be82b28105c7
shady-wern <nikl.wern.work@gmail.com>
2023-03-21 14:49:07 +0300

    lab2_final

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,7 +1,13 @@
 package ru.iu3.fclient;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -9,11 +15,10 @@ import android.view.View;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
-
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityResultLauncher activityResultLauncher;
     // Used to load the 'fclient' library on application startup.
     static {
         System.loadLibrary("fclient");
@@ -34,6 +39,20 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_button);
         tv.setText(stringFromJNI());
+
+        activityResultLauncher  = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+                            // обработка результата
+                            String pin = data.getStringExtra("pin");
+                            Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     /**
@@ -60,11 +79,14 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonClick(View v)
     {
-        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+        /*/byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
         byte[] enc = encrypt(key, stringToHex("000000000000000102"));
         byte[] dec = decrypt(key, enc);
         String s = new String(Hex.encodeHex(dec)).toUpperCase();
-        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();/*/
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);
     }
     public static native byte[] encrypt(byte[] key, byte[] data);
     public static native byte[] decrypt(byte[] key, byte[] data);
--- fclient/app/src/main/java/ru/iu3/fclient/PinpadActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/PinpadActivity.java
@@ -2,7 +2,9 @@ package ru.iu3.fclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -22,6 +24,9 @@ public class PinpadActivity extends AppCompatActivity {
         ShuffleKeys();
 
         findViewById(R.id.btnOK).setOnClickListener((View) -> {
+            Intent it = new Intent();
+            it.putExtra("pin", pin);
+            setResult(RESULT_OK, it);
             finish();
         });
 
@@ -30,6 +35,19 @@ public class PinpadActivity extends AppCompatActivity {
             tvPin.setText("");
         });
     }
+
+
+    public void keyClick(View v)
+    {
+        String key = ((TextView)v).getText().toString();
+        int sz = pin.length();
+        if (sz < 4)
+        {
+            pin += key;
+            tvPin.setText("****".substring(3 - sz));
+        }
+    }
+
 
     protected void ShuffleKeys()
     {
