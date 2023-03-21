dd993fe820508b0e2512660701fb2b03ff261110
shady-wern <nikl.wern.work@gmail.com>
2023-03-21 14:20:27 +0300

    lab2_extended

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
-
-
-import com.google.android.filament.View;
+import android.view.View;
 
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
@@ -62,8 +60,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonClick(View v)
     {
+        byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+        byte[] dec = decrypt(key, enc);
+        String s = new String(Hex.encodeHex(dec)).toUpperCase();
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
     }
-
-
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
