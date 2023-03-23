802fbe8fd46198d34f55d903485caf346638fa92
shady-wern <nikl.wern.work@gmail.com>
2023-03-23 05:27:55 +0300

    lab4

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -1,4 +1,3 @@
-
 package ru.iu3.fclient;
 
 import androidx.activity.result.ActivityResult;
@@ -8,26 +7,30 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.codec.DecoderException;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.codec.binary.Hex;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.commons.io.IOUtils;
 
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Hex;
-
-
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import ru.iu3.fclient.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements TransactionEvents{
-    ActivityResultLauncher activityResultLauncher;
     // Used to load the 'fclient' library on application startup.
     static {
         System.loadLibrary("fclient");
-        //System.loadLibrary("native-lib");
         System.loadLibrary("mbedcrypto");
     }
     private String pin;
@@ -48,47 +51,86 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
         }
         return pin;
     }
-    private ActivityMainBinding binding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    //public void onButtonClick(View v)
+    // {
+    //    Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+    //}
+    //public void onButtonClick(View v)
+    //{
+    //    byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
+    //    byte[] enc = encrypt(key, stringToHex("000000000000000102"));
+    //    byte[] dec = decrypt(key, enc);
+    //    String s = new String(Hex.encodeHex(dec)).toUpperCase();
+    //    Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    //}
 
 
-
-
-
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        activityResultLauncher  = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-                            // обработка результата
-                            //String pin = data.getStringExtra("pin");
-                            //Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
-                            pin = data.getStringExtra("pin");
-                            synchronized (MainActivity.this) {
-                                MainActivity.this.notifyAll();
-                            }
-                        }
-                    }
+    protected void testHttpClient()
+    {
+        new Thread(() -> {
+            try {
+                HttpURLConnection uc = (HttpURLConnection)
+                        (new URL("http://192.168.0.187:8080/api/v1/title").openConnection());
+                InputStream inputStream = uc.getInputStream();
+                String html = IOUtils.toString(inputStream);
+                String title = getPageTitle(html);
+                runOnUiThread(() ->
+                {
+                    Toast.makeText(this, title, Toast.LENGTH_LONG).show();
                 });
 
-        int res = initRng();
-        byte[] v = randomBytes(10);
-
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_button);
-        tv.setText(stringFromJNI());
-
+            } catch (Exception ex) {
+                Log.e("fapptag", "Http client fails", ex);
+            }
+        }).start();
     }
+
+
+
+    protected String getPageTitle(String html)
+    {
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>", Pattern.DOTALL);
+        Matcher matcher = pattern.matcher(html);
+        String p;
+        if (matcher.find())
+            p = matcher.group(1);
+        else
+            p = "Not found";
+        return p;
+    }
+
+
+
+
+
+
+
+    public void onButtonClick(View v)
+    {
+        testHttpClient();
+        //byte[] trd = stringToHex("9F0206000000000100");
+        //transaction(trd);
+
+
+        /*new Thread(()-> {
+            try {
+                byte[] trd = stringToHex("9F0206000000000100");
+                boolean ok = transaction(trd);
+                runOnUiThread(()-> {
+                    Toast.makeText(MainActivity.this, ok ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+                });
+            } catch (Exception ex) {
+                // todo: log error
+            }
+        }).start();
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        activityResultLauncher.launch(it);*/
+    }
+
+
+
 
     public static byte[] stringToHex(String s)
     {
@@ -105,37 +147,44 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
     }
 
 
+    private ActivityMainBinding binding;
+
+    ActivityResultLauncher activityResultLauncher;
 
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-    public void onButtonClick(View v)
-    {
-       /* byte[] key = stringToHex("0123456789ABCDEF0123456789ABCDE0");
-        byte[] enc = encrypt(key, stringToHex("000000000000000102"));
-        byte[] dec = decrypt(key, enc);
-        String s = new String(Hex.encodeHex(dec)).toUpperCase();
-        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
-        Intent it = new Intent(this, PinpadActivity.class);
-        startActivity(it);
-        activityResultLauncher.launch(it);*/
+        int res=initRng();
+        byte[] v = randomBytes(10);
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
 
-        new Thread(()-> {
-            try {
-                byte[] trd = stringToHex("9F0206000000000100");
-                boolean ok = transaction(trd);
-                /*
-                runOnUiThread(()-> {
-                    Toast.makeText(MainActivity.this, ok ? "ok" : "failed", Toast.LENGTH_SHORT).show();
-                });*/
+        // Example of a call to a native method
+        //TextView tv = binding.sampleText;
+        //TextView tv = findViewById(R.id.sample_text);
 
-            } catch (Exception ex) {
-                // todo: log error
-            }
-        }).start();
-
-
-
+        //tv.setText(stringFromJNI());
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Intent data = result.getData();
+// обработка результата
+                            //String pin = data.getStringExtra("pin");
+                            //Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                            pin = data.getStringExtra("pin");
+                            synchronized (MainActivity.this) {
+                                MainActivity.this.notifyAll();
+                            }
+                        }
+                    }
+                });
     }
 
 
@@ -148,24 +197,15 @@ public class MainActivity extends AppCompatActivity implements TransactionEvents
 
 
 
-
-
-    public static native byte[] encrypt(byte[] key, byte[] data);
-    public static native byte[] decrypt(byte[] key, byte[] data);
-
-
-
-
-
     /**
      * A native method that is implemented by the 'fclient' native library,
      * which is packaged with this application.
      */
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
     public native String stringFromJNI();
     public static native int initRng();
     public static native byte[] randomBytes(int no);
-
-
     public native boolean transaction(byte[] trd);
 
 
